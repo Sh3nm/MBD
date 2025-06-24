624a410212ca988f1e0eb8aafdec979a2a2bef34
Shenmue <162028242+Sh3nm@users.noreply.github.com>
2025-06-24 12:30:41 +0700

    ngubah dikit di index.js

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const pool = require('./db');
 require('dotenv').config();
+const cors = require('cors'); 
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 3333; // Ndatau kenapa port 3000 kepake java.exe
 
 app.get('/', (req, res) => {
   res.send('Server is running!');
 });
+app.use(express.json());
 
 app.post('/add-flight', async (req, res) => {
   const {
